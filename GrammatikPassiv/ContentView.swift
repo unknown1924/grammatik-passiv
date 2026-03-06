3a38316c323ae6aab0fd5db776863225206c47c0
Debasis Mandal <debasismandal900@gmail.com>
2026-03-06 17:35:33 +0530

    Initial Commit

---
+++ GrammatikPassiv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrammatikPassiv
+//
+//  Created by Debasis Mandal on 06/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrammatikPassiv/GrammatikPassivApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammatikPassivApp.swift
+//  GrammatikPassiv
+//
+//  Created by Debasis Mandal on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GrammatikPassivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
