d38d623d235ccedea0f27c767473612a4aa53b41
Debasis Mandal <debasismandal900@gmail.com>
2026-07-22 02:18:01 +0530

    Implemented firebase email/password authentication and FirebaseUI.

--- GrammatikPassiv/GrammatikPassivApp.swift
+++ GrammatikPassiv/GrammatikPassivApp.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct GrammatikPassivApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
